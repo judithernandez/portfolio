69d0679076f66501030c45e4eaa661ac6f66e365
Judith <judith@earthpulse.ai>
2024-03-16 15:13:10 +0100

    pulses doc

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1> Hello from my Portfolio!</h1>
+      <h1> Hello from my Portfolio, my name is Judith!</h1>
       </main>
   )
 }
